f9542be606f0959c4fc1cd691cc66f52280586a9
Ken Ng <kenngsimply@gmail.com>
2017-12-04 15:50:40 +0800

    manage to receive push notification

---
+++ curl-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -X POST -H "Authorization: key=<API_ACCESS_KEY>" -H "Content-Type: application/json" \
+   -d '{
+  "notification": {
+    "title": "FCM Message",
+    "body": "This is an FCM Message",
+    "icon": "/itwonders-web-logo.png",
+  },
+  "to": "<DEVICE_REGISTRATION_TOKEN>"
+}' https://fcm.googleapis.com/fcm/send
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': 'YOUR-SENDER-ID'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/itwonders-web-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
