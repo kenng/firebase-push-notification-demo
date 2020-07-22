a7ab71ecd4228fdb9dfacd14c3f71879c1f5a68e
Ken Ng <kenng@itwonders.co>
2020-07-23 00:25:11 +0800

    Update to firebase 7.16, add foreground notis

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,19 @@
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
-// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics. 
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js');
+importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+);
+// For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+importScripts(
+    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+);
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+    messagingSenderId: "YOUR-SENDER-ID",
+    apiKey: "YOUR_API_KEY",
+    projectId: "YOUR_PROJECT_ID",
+    appId: "YOUR_APP_ID",
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -14,14 +21,19 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/itwonders-web-logo.png'
-  };
+    console.log(
+        "[firebase-messaging-sw.js] Received background message ",
+        payload,
+    );
+    // Customize notification here
+    const notificationTitle = "Background Message Title";
+    const notificationOptions = {
+        body: "Background Message body.",
+        icon: "/itwonders-web-logo.png",
+    };
 
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions,
+    );
 });
